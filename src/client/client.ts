8636e3f5e384df295401890ed220139b4e36c547
TurtleWolfe <JonPohlner@gmail.com>
2021-02-22 16:22:45 -0500

    #18 Stats Panel

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,8 +1,12 @@
 import * as THREE from '/build/three.module.js'
 import { OrbitControls } from '/jsm/controls/OrbitControls'
+import Stats from '/jsm/libs/stats.module'
 
 const scene: THREE.Scene = new THREE.Scene()
-
+// red
+// scene.background = new THREE.Color(0xFF0000)
+// grey
+scene.background = new THREE.Color(0x808080)
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
@@ -10,6 +14,7 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
+controls.addEventListener('change', render) //this line is unnecessary if you are re-rendering within the animation loop 
 
 const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
 const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
@@ -17,17 +22,35 @@ const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0
 const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-camera.position.z = 50
+camera.position.z = 2
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
+
+const stats = Stats()
+document.body.appendChild(stats.dom)
 
 var animate = function () {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 
-    controls.update()
+    render()
 
-    renderer.render(scene, camera)
+    stats.update()
 };
 
-animate();
+
+function render() {
+    stats.begin()
+    renderer.render(scene, camera)
+    stats.end()
+}
+render()
+//animate();
